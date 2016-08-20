7b065df2c3cc9dfdf6f364fbf0fe05949911605e
Harendra Kumar <harendra.kumar@gmail.com>
2016-08-20 13:13:13 +0530

    Add libxls 1.4.0 source files

---
+++ lib/libxls/config/config.h
@@ -0,0 +1,8 @@
+/* Define to 1 if you have the `asprintf' function. */
+#define HAVE_ASPRINTF 1
+
+/* Define if you have the iconv() function. */
+#define HAVE_ICONV 1
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.4.0"
