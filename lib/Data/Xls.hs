2a8101123571c5b10f37560667f4e5ae75b1d6a5
Harendra Kumar <harendra.kumar@gmail.com>
2016-09-18 10:30:40 +0530

    Use gcc instead of cc for preprocessing
    Because Windows does not have a cc. This makes it portable only to gcc now.

--- lib/Data/Xls.hs
+++ lib/Data/Xls.hs
@@ -9,7 +9,7 @@
 --
 -- Parse Microsoft excel spreadsheet xls file (format BIFF/Excel 97-2004).
 --
-{-# OPTIONS_GHC -pgmP cc -optP -E -optP -undef -optP -std=c89 #-}
+{-# OPTIONS_GHC -pgmP gcc -optP -E -optP -undef -optP -std=c89 #-}
 
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE EmptyDataDecls #-}
