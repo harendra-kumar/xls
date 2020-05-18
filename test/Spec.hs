cdae019b59f975308dab5089575e316d3acaebec
Artem Kanev <a-kanev@yandex.ru>
2020-05-18 09:41:27 +0300

    Basic sanity check.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+import           Data.Xls
+import           Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ describe "Sanity check" $ do
+    it "Test file parsing" $ do
+        content <- decodeXlsIO "test/data/test.xls"
+        content `shouldBe` testFileContent
+
+testFileContent :: [[[String]]]
+testFileContent = [[["1.000000000000000","2.3","text"]],[["1.000000000000000","2.3","text"]]]
