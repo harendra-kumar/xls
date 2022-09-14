4925ca760f57052a911eb6e8392a396e7f7525e6
olafklinke <84761329+olafklinke@users.noreply.github.com>
2022-09-14 15:27:03 +0200

    add functions to decode ByteStrings

--- lib/Data/Xls.hs
+++ lib/Data/Xls.hs
@@ -21,22 +21,32 @@
 
 module Data.Xls
     ( decodeXlsIO
+    , decodeXlsByteString
+    , decodeXlsByteString'
     , decodeXls
     , XlsException(..)
+    , XLSError(..)
     )
 where
 
-import           Control.Exception (Exception, throwIO, bracket)
+import           Control.Exception (Exception, throwIO, bracket, catch)
 import           Control.Monad.IO.Class
-import           Control.Monad (when, void)
+import           Control.Monad (void)
 import           Control.Monad.Trans.Resource
 import           Data.Conduit hiding (Conduit, Sink, Source)
 import           Data.Data
 import           Data.Int
+import           Data.Word (Word32)
 import           Data.Maybe (catMaybes, fromJust, isJust, fromMaybe)
+import           Data.ByteString (hPut)
+import           Data.ByteString.Internal (ByteString(..))
 import           Foreign.C
 import           Foreign.Ptr
+import           Foreign.ForeignPtr (withForeignPtr)
+import           Foreign.Storable (Storable(..))
+import           Foreign.Marshal.Alloc (malloc)
 import           Text.Printf
+import           System.IO.Temp (withSystemTempFile)
 
 #define CCALL(name,signature) \
 foreign import ccall unsafe #name \
@@ -44,9 +54,43 @@ foreign import ccall unsafe #name \
 
 -- Workbook accessor functions
 data XLSWorkbookStruct
+-- | An enum returned by libxls. See libxls\/include\/xls.h
+data XLSError = LIBXLS_OK 
+    | LIBXLS_ERROR_OPEN 
+    | LIBXLS_ERROR_SEEK 
+    | LIBXLS_ERROR_READ 
+    | LIBXLS_ERROR_PARSE 
+    | LIBXLS_ERROR_MALLOC
+    deriving (Show,Eq,Enum)
+instance Storable XLSError where
+    sizeOf = const (sizeOf (0 :: Word32)) -- TODO: sizeof enum is compiler and architecture dependent!
+    alignment = sizeOf -- okay for simple Storables
+    peek = fmap (toEnum . (fromIntegral :: Word32 -> Int)) . peek . castPtr
+    poke ptr e = poke (castPtr ptr) ((fromIntegral :: Int -> Word32).fromEnum $ e)
 type XLSWorkbook = Ptr XLSWorkbookStruct
+type XLSErrorT = Ptr XLSError
+type CBuffer = Ptr CUChar
+
+-- | Recall that
+-- @
+-- ByteString ~ (ForeignPtr Char8,Int)
+--     CUChar ~ Word8
+--      CSize ~ Word64
+-- @
+--
+-- So we need to marshal
+-- 
+-- @
+-- (ForeignPtr Char8) -> Ptr CUChar
+-- Int -> CSize
+-- @
+toCBuffer :: ByteString -> IO (CBuffer,CSize)
+toCBuffer (PS fPtr offset ilen) = do
+    withForeignPtr fPtr $ \ptrData -> do
+        return (plusPtr (castPtr ptrData) offset,CSize (fromIntegral ilen))
 
 CCALL(xls_open,          CString -> CString -> IO XLSWorkbook)
+CCALL(xls_open_buffer,   CBuffer -> CSize -> CString -> XLSErrorT -> IO XLSWorkbook)
 CCALL(xls_wb_sheetcount, XLSWorkbook -> IO CInt -- Int32)
 CCALL(xls_close_WB,      XLSWorkbook -> IO ())
 
@@ -82,6 +126,13 @@ data XlsException =
 
 instance Exception XlsException
 
+exceptionLeft :: XlsException -> Either XLSError a
+exceptionLeft (XlsFileNotFound _) = Left LIBXLS_ERROR_OPEN
+exceptionLeft (XlsParseError   _) = Left LIBXLS_ERROR_PARSE
+
+catchXls :: IO a -> IO (Either XLSError a)
+catchXls = flip catch (return.exceptionLeft) . fmap Right 
+
 -- | Parse a Microsoft excel xls workbook file into a Conduit yielding
 -- rows in a worksheet. Each row represented by a list of Strings, each String
 -- representing an individual cell.
@@ -111,6 +162,27 @@ decodeXls file =
             count <- liftIO $ c_xls_wb_sheetcount pWB
             mapM_ (decodeOneWorkSheet file pWB) [0 .. count - 1]
 
+-- | A work-around via temporary files and 'decodeXlsIO', 
+-- since this lib is lacking a pure function to decode the contents 
+-- of an XLS file. 
+-- Due to Erik Rybakken. 
+decodeXlsByteString :: ByteString -> IO [[[String]]]
+decodeXlsByteString content = withSystemTempFile "decodeXlsByteString"
+    $ \filePath h -> do
+        hPut h content
+        decodeXlsIO filePath
+
+-- | Experimental: This function uses the @xls_open_buffer@ function of libxls.
+decodeXlsByteString' :: ByteString -> IO (Either XLSError [[[String]]])
+decodeXlsByteString' bs = do
+    (buf,buflen) <- toCBuffer bs
+    enc <- newCString "UTF-8"
+    outError <- malloc
+    wb <- c_xls_open_buffer buf buflen enc outError
+    e <- peek outError
+    case e of
+        LIBXLS_OK -> decodeXLSWorkbook Nothing wb
+        _         -> return (Left e) 
 
 -- | Parse a Microsoft excel xls workbook file into a list of worksheets, each
 -- worksheet consists of a list of rows and each row consists of a list of
@@ -124,13 +196,25 @@ decodeXlsIO
 decodeXlsIO file = do
     file' <- newCString file
     pWB <- newCString "UTF-8" >>= c_xls_open file'
-    when (pWB == nullPtr) $
-        throwIO $ XlsFileNotFound
-                $ "XLS file " ++ file ++ " not found."
-    count <- liftIO $ c_xls_wb_sheetcount pWB
-    results <- mapM (decodeOneWorkSheetIO file pWB) [0 .. count - 1]
-    void $ c_xls_close_WB pWB
-    return results
+    parseResult <- decodeXLSWorkbook (Just file) pWB
+    case parseResult of
+        Right results -> return results
+        Left  e -> case e of
+            LIBXLS_ERROR_OPEN -> throwIO $ XlsFileNotFound $ 
+                "XLS file " ++ file ++ " not found."
+            _                 -> throwIO $ XlsParseError $ 
+                "XLS file " ++ file ++ " could not be parsed."
+
+-- helper function for decoding both file and buffer
+decodeXLSWorkbook :: Maybe FilePath -> XLSWorkbook -> IO (Either XLSError [[[String]]])
+decodeXLSWorkbook mFile pWB = if pWB == nullPtr
+    then return (Left LIBXLS_ERROR_OPEN)
+    else catchXls $ do 
+        count <- liftIO $ c_xls_wb_sheetcount pWB
+        results <- mapM (decodeOneWorkSheetIO (maybe "buffer" id mFile) pWB) [0 .. count - 1]
+        void $ c_xls_close_WB pWB
+        return results
+    
 
 decodeOneWorkSheet
     :: MonadResource m
